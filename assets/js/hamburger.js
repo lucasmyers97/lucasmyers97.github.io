6412fdab998280458ce5f8162b31363ea02ebec3
Lucas Myers <lucasmyers97@gmail.com>
2023-08-19 19:49:25 -0500

    Can click away from sidebar, but can't click links

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,5 +1,10 @@
 function clickHamburger() {
-    // const sidebar_element = document.getElementById('sidebar');
-    document.getElementById('sidebar').setAttribute('class', 'sidebar-open');
-    // sidebar_element.focus();
+    const sidebar_element = document.getElementById('sidebar');
+    sidebar_element.setAttribute('class', 'sidebar-open');
+    sidebar_element.focus();
+    console.log("I clickedd on the hamburger");
+    sidebar_element.addEventListener('blur', (event) => {
+        sidebar_element.setAttribute('class', 'sidebar-closed');
+        console.log("I did it");
+    }, true);
 }
