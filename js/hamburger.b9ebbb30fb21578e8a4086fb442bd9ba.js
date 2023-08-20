9a315a4de4cc211e35d8e4c6e429e0a479febacc
lucasmyers97 <lucasmyers97@users.noreply.github.com>
2023-08-20 04:58:30 +0000

    deploy: cf02d3cfc7178621bb57af1a3eb6266b1d2006a1

---
+++ js/hamburger.b9ebbb30fb21578e8a4086fb442bd9ba.js
@@ -0,0 +1,21 @@
+function clickHamburger() {
+
+    const sidebar_element = document.getElementById('sidebar');
+    sidebar_element.setAttribute('class', 'sidebar-open');
+    sidebar_element.focus();
+
+    sidebar_element.addEventListener('focusout', (event) => {
+        if (event.relatedTarget) {
+            if (sidebar_element.contains(event.relatedTarget)) {
+                return;
+            }
+        }
+        sidebar_element.dataset.timeout = setTimeout(() => {
+            sidebar_element.setAttribute('class', 'sidebar-closed');
+        }, 0)
+    });
+
+    sidebar_element.addEventListener('focus', (event) => {
+        clearTimeout(sidebar_element.dataset.timeout);
+    });
+}
