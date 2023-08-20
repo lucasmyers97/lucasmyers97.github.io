7a6990e059403a76c76b43a874e0490ef88a5c0a
Lucas Myers <lucasmyers97@gmail.com>
2023-08-19 19:13:32 -0500

    Styled hamburger, tried to make sidebar animate

---
+++ assets/js/hamburger.js
@@ -0,0 +1,5 @@
+function clickHamburger() {
+    // const sidebar_element = document.getElementById('sidebar');
+    document.getElementById('sidebar').setAttribute('class', 'sidebar-open');
+    // sidebar_element.focus();
+}
