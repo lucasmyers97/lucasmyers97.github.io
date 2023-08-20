9c22532eff922c35b218ceb452f2880fd1ba95bc
Lucas Myers <lucasmyers97@gmail.com>
2023-08-20 11:47:47 -0500

    Think I fixed the sidebar animation on rotation

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,7 +1,24 @@
+const openSidebar = [
+    { transform: 'translate(0)' },
+    { transform: 'translate(-280px)' }
+];
+
+const closeSidebar = [
+    { transform: 'translate(0)' },
+    { transform: 'translate(280px)' }
+];
+
+const sidebarTiming = {
+  duration: 150,
+  iterations: 1,
+};
+
 function clickHamburger() {
 
     const sidebar_element = document.getElementById('sidebar');
-    sidebar_element.setAttribute('class', 'sidebar-open');
+    sidebar_element.animate(openSidebar, sidebarTiming).finished.then(() => {
+        sidebar_element.setAttribute('class', 'sidebar-open');
+    });
     sidebar_element.focus();
 
     sidebar_element.addEventListener('blur', (event) => {
@@ -11,7 +28,9 @@ function clickHamburger() {
             }
         }
         sidebar_element.dataset.timeout = setTimeout(() => {
-            sidebar_element.setAttribute('class', 'sidebar-closed');
+            sidebar_element.animate(closeSidebar, sidebarTiming).finished.then(() => {
+                sidebar_element.setAttribute('class', 'sidebar-closed');
+            });
         }, 0)
     });
 
